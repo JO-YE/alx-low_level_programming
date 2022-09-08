6ad1a55b6769045f24dd73b4ccfb697eae0f070b
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-08 16:42:00 -0700

    writing c program with the printf fxn, followed my a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * maon - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art\n");
+	return(0);
+}
