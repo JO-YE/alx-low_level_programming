35730d4c038d9e3d269f1f685337e00966d0e84b
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-10 05:29:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,9 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19,\n", 58);
+
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+
+	write(2, str1, 59);
 	return (1);
 }
