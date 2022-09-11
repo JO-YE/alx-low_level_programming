403d622259ec81479a68c8feb98c30760be40216
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-10 21:04:30 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /** 
- * main - Determines if a number is positive, negative or zero
+ * main - Determines if a number is positive nagative or zero
  *
  * Return: Always 0 (success)
  */
