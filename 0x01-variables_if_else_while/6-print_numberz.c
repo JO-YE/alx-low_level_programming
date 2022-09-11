9956838038a29ed4df67270a1ba1ce4be5478417
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-11 07:32:00 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Print digit number from 10 to 0 by using only putchar.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int b;
+	
+	for (b = 48; b < 58; b++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
