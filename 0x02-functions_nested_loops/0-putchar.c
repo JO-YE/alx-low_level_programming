3049c3dab6afdd5358b9352a88fe359b771923bc
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-13 12:22:46 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - putchar write the a character to stdout.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+		{
+			putchar(ch);
+			ch++;
+		}
+
+	return (0);
+}
