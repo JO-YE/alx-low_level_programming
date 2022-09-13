23823deb7fefd682ec863171b2724755bf9ad967
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-13 15:27:21 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * Print_alphabet - Print alphabet in lower case *10
+ *
+ * Return: always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i, mul;
+
+	for (i = 97; i < 123; i++)
+	{
+	_putchar(i);
+	mul = i * 10;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define _MAIN_H_
 int _putchar (char ch);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif /* _MAIN_H_ */
