9bcadd937933d73aebeabf71504271f4a56681cb
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-13 09:15:46 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -29,7 +29,7 @@ int main(void)
 						putchar(' ');
 						putchar(e);
 						putchar(i);
-						if (h == 57 && g == 56 && e == 57 && i == 57)
+						else (h == 57 && g == 56 && e == 57 && i == 57)
 						break;
 						putchar(',');
 						putchar(' ');
