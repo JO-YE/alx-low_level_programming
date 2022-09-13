ad3628214233afc24a0713b235d991a2f507ee19
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-13 14:34:47 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 int _putchar (char ch);
+void print_alphabet(void);
 #endif /* _MAIN_H_ */
