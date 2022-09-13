709f708052a81715f35929fc305b4930d0acb2a6
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-13 14:21:32 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,14 @@
  */
 int main(void)
 {
-	char ch;
-	ch = getchar ();
-	_putchar (ch);
+	char *ch = "_putchar";
+
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	_putchar ('\n');
+
 	return (0);
 }
