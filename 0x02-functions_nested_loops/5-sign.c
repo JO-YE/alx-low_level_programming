3a3de5996a8c5c8d257b7bcd1a5ab487c0b64759
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-13 23:22:51 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,20 @@
  */
 int print_sign(int n)
 {
-	if (n > 48)
+	if (n > 0)
 	{
+		_putchar(49);
 		return (1);
 	}
-	else if (n == 48)
+	else if (n < 0)
 	{
-		return (0);
+		_putchar(46);
+		return (-1);
 	}
 	else
 	{
-		return (-1);
+		_putchar(48);
+		return (0);
 	}
 	_putchar('\n');
 }
