96b197d4488d435da865e71eb1ff1c126a642e90
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-15 22:44:11 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9 /n
+ *
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
