476d7757deb563ebeb8c26da2c9cd59fecc69460
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-09-20 11:51:00 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter
- * and updates the value it points to to 98
+ * and updates the value.
  * @n: input integar
  * Return: no return
  */
