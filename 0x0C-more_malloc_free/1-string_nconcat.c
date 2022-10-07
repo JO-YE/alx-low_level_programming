d0a4970865b8e2fa0c039392217563de3c87e91f
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-10-06 17:33:47 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,37 +6,38 @@
  * @s1: first string
  * @s2: second string
  * @n: number of bytes
- * Return: pointer to the allocated memory
- * if malloc fails, status value is equal to 98.
+ * Return: return a char val
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+	unsigned int i, len1, len2;
+	char *s;
 
-	if (s1 == NULL)
+	if (s2 == NULL)
 		s2 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
-	if (n > ls2)
-		n = ls2;
+	len1 = 0;
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
+	while (s1[len1] != '\0')
+		len1++;
 
-	lsout = ls1 + n;
-
-	sout = malloc(lsout + 1);
-
-	if (sout == NULL)
+	if (n >= len2)
+		n = len2;
+	s = malloc(sizeof(char) * n + len1 + 1);
+	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
-	sout[i] = '\0';
+	for (i = 0; i < len1; i++)
+		s[i] = s1[i];
 
-	return (sout);
+	for (i = 0; i < n; i++)
+		s[i + len1] = s2[i];
+
+	s[i + len1] = '\0';
+
+	return (s);
 }
