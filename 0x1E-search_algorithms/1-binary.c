d5085f2aae165abf718bf18db5572f491711d291
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-04-12 23:47:10 +0100

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -6,8 +6,28 @@
 * @size: is the number of elements in array
 * @value: is the value to search in array
 * 
-* Return: the value else -1
+* Return: return the value else -1
 */
 
-int binary_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value)
+{
+	size_t low = 0, mid, high = size - 1;
 
+	if (array == NULL)
+		return (-1);
+
+	while (low != high)
+	{
+		mid = (low + high) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	/* egde cases condition */
+	if (array[high] == value)
+		return (high);
+	return(-1);
+}
