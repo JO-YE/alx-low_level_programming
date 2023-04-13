5b425dc730fadf54bd909ac4019c879f93658617
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-04-13 22:51:14 +0100

    time complexity worst case

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void prt_array(int *array, size_t low, size_t high);
 int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 
