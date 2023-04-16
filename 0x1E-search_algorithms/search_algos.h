35e7fd0b036bd500524209b3c79513fd6633a960
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-04-16 20:10:54 +0100

    function that searches for a value using jump search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,10 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 void prt_array(int *array, size_t low, size_t high);
 int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
-
+int jump_search(int *array, size_t size, int value);
 
 #endif
