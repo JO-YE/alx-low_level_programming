3e0c53f406a03db5517a3e1b9afae89a7ba9a4e0
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-04-17 19:07:42 +0100

    function that searches for a value using jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -44,14 +44,8 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
 		if (array[prev] == value)
-			break;
+			return (prev);
 		prev++;
-	}
-
-
-	if (array[prev] == value)
-	{
-		return (prev);
 	}
 	return (-1);
 }
